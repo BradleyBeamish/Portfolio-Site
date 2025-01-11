814e3f11362d2dea4580568f3015b80092f19ade
BradleyBeamish <bbeam5123@gmail.com>
2025-01-11 10:52:05 -0500

    Website refresh

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -14,24 +14,18 @@ function Intro() {
             <p className="pb-3 pt-5 sm:pb-5 text-base md:text-xl font-medium drop-shadow-lg shadow-black">
                 Sotware Developer
             </p>
-            <p className="pb-3 sm:pb-5 px-4 sm:px-2 md:px-0 text-sm max-w-xl font-bold drop-shadow-lg shadow-black">
-                As a student at Algonquin College in Ottawa, I am constantly
-                learning and expanding my skillset as a programmer. I have
-                experience working with a wide variety of languages in the
-                Frontend and Backend, including proficiency in Object-Oriented
-                programming. I am passionate about using my coding skills to
-                build innovative and functional projects.
-            </p>
             <a
                 href="https://github.com/BradleyBeamish"
                 target="_blank"
-                rel="noopener norefferer"
+                rel="noopener noreferrer"
+                className="flex items-center space-x-2"
             >
                 <img
                     src="assets/github-mark.svg"
                     alt="logo"
                     className="object-cover cursor-pointer h-10"
                 />
+                <p>Projects</p>
             </a>
         </div>
     );
